cbf6c54c982819b051683fe11e855324a0c47ce8
Alibek <sultanovalibek2020@gmail.com>
2026-02-16 18:20:04 +0600

    Commit_4:Add client storage and broadcast

--- EchoServer/src/ClientHandler.java
+++ EchoServer/src/ClientHandler.java
@@ -4,9 +4,12 @@ import java.net.Socket;
 public class ClientHandler implements Runnable {
 
     private final Socket socket;
+    private final EchoServer server;
+    private PrintWriter out;
 
-    public ClientHandler(Socket socket) {
+    public ClientHandler(Socket socket, EchoServer server) {
         this.socket = socket;
+        this.server = server;
     }
 
     @Override
@@ -15,13 +18,31 @@ public class ClientHandler implements Runnable {
                 BufferedReader in = new BufferedReader(
                         new InputStreamReader(socket.getInputStream()))
         ) {
+            out = new PrintWriter(socket.getOutputStream(), true);
+
             String line;
             while ((line = in.readLine()) != null) {
-                System.out.println("Message: " + line);
+                server.broadcast(line, this);
             }
 
         } catch (IOException e) {
             System.out.println("Client disconnected");
+        } finally {
+            close();
         }
+    }
+
+    public void send(String message) {
+        if (out != null) {
+            out.println(message);
+        }
+    }
+
+    private void close() {
+        try {
+            socket.close();
+        } catch (IOException ignored) {}
+
+        server.removeClient(this);
     }
 }
--- EchoServer/src/EchoServer.java
+++ EchoServer/src/EchoServer.java
@@ -1,10 +1,13 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
 
 public class EchoServer {
 
     private final int port;
+    private final Set<ClientHandler> clients = new HashSet<>();
 
     public EchoServer(int port) {
         this.port = port;
@@ -17,13 +20,27 @@ public class EchoServer {
             while (true) {
                 Socket socket = serverSocket.accept();
 
-                ClientHandler client = new ClientHandler(socket);
+                ClientHandler client = new ClientHandler(socket, this);
+                clients.add(client);
+
                 new Thread(client).start();
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void broadcast(String message, ClientHandler sender) {
+        for (ClientHandler client : clients) {
+            if (client != sender) {
+                client.send(message);
+            }
+        }
+    }
+
+    public void removeClient(ClientHandler client) {
+        clients.remove(client);
     }
 
     public static void main(String[] args) {
