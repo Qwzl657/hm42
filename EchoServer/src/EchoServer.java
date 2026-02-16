bd509510f95e181df9b928d8374d543052d0211b
Alibek <sultanovalibek2020@gmail.com>
2026-02-16 18:08:27 +0600

    Commit_1.1:Server accept connection

--- EchoServer/src/EchoServer.java
+++ EchoServer/src/EchoServer.java
@@ -1,44 +1,30 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class EchoServer {
 
     private final int port;
-    private final ExecutorService pool = Executors.newCachedThreadPool();
 
-    private EchoServer(int port) {
+    public EchoServer(int port) {
         this.port = port;
     }
 
-    public static EchoServer bindToPort(int port) {
-        return new EchoServer(port);
-    }
-
     public void run() {
-
-        try (ServerSocket server = new ServerSocket(port)) {
-
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server started on port " + port);
 
-            while (!server.isClosed()) {
-
-                Socket socket = server.accept();
-
-                ClientHandler handler =
-                        new ClientHandler(socket, this);
-
-                pool.submit(handler);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected: " + socket);
             }
 
         } catch (IOException e) {
-            System.out.println("Port busy.");
+            e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        EchoServer.bindToPort(8089).run();
+        new EchoServer(12345).run();
     }
 }
